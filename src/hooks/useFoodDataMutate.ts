1fa968119b95d82dcb6b86a2c571df4e9fc42264
nathaliaporcelli <nathalia_porcelli@hotmail.com>
2024-05-18 14:18:48 -0300

    ajuste link backend

--- src/hooks/useFoodDataMutate.ts
+++ src/hooks/useFoodDataMutate.ts
@@ -2,10 +2,10 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios, { AxiosPromise } from "axios";
 import { FoodData } from "../interface/FoodData";
 
-const API_URL = 'http://spring_cardapio:8080';
+const API_URL = 'http://localhost:8080';
 
 const postData = async (data: FoodData): AxiosPromise<any> => {
-    const response = axios.post(API_URL + '/cardapio', data);
+    const response = axios.post(API_URL + '/burgers', data);
     return response;
 }
 
